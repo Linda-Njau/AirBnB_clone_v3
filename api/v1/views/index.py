2a4bd1b3d63ac1faa26c8c8d89d785e24fb60dae
Linda-Njau <lindanjau21@.com>
2023-05-01 11:24:10 +0300

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,16 @@ from flask import jsonify
 @app_views.route('/status')
 def status():
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/api/v1/stats', method=['GET'])
+def get_stats():
+    Stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count(User)
+    }
+    return jsonify(stats)
