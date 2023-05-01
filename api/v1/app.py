0fbdbb2dd20f6173e24208ee712b0ec8ff8ad371
Linda-Njau <lindanjau21@.com>
2023-05-01 11:12:47 +0300

    pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,13 +9,14 @@ from models import storage
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.teardown_appcontext
 def close_storage(exception):
     storage.close()
-    
+
+
 if __name__ == "__main__":
     import os
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
-
