b616240483b2df9a0a4d3850f28564b6514eb923
Linda-Njau <lindanjau21@.com>
2023-05-01 17:06:32 +0300

    state

---
+++ api/v1/views/states.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+States view for RESTful API.
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models import storage
+from models.state import State
+
+@app_views.route('/states', methods=['GET'])
+def get_states():
+    """Retrieve the list of all State objects."""
+    states = storage.all('State').values()
+    return jsonify([state.to_dict() for state in states])
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    """Retrieve a State object based on the id."""
+    state = storage.get('state', state_id)
+    if not state:
+        abort(404)
+    return jsonify(state.to_dict())
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def delete_state(state_id):
+    """Deletes a State object based on id."""
+    state = storage.get('State', state_id)
+    if not state:
+        abort(404)
+    storage.delete(state)
+    storage.save()'
+    return jsonify({}), 200
+
+@app_views.route('/states', methods-['POST'])
+def create_state():
+    """Creates a State object from json data."""
+    data = request.get_json()
+    if  data is None:
+        abort(400, 'Not a JSON')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    state = State(**data)
+    state.save()
+    return jsonify(state.to_dict()), 201
+
+@app_views.route('/states/<state_id', methods=['PUT'])
+def x.update_state(state_id):
+    """Updates a State object based on id."""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    ignore_keys = ['id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict()), 200    
