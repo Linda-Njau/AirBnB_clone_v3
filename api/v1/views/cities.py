f286ef34167f6c947e721e9e8cb6a793c3f93931
Linda-Njau <lindanjau21@.com>
2023-05-02 04:30:50 +0300

    cities.py

---
+++ api/v1/views/cities.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+city API
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.city import City
+from models.state import State
+
+
+@app_views.route('/states/<state_id>/cities', methods=['GET'])
+def get_cities_by_state(state_id):
+    """returns dictionary of all cities in a state by the state id"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    cities = [city.to_dict() for city in state.cities]
+    return jsonify(cities)
+
+
+@app_views.route('/cities/<city_id>', methods=['GET'])
+def get_city(city_id):
+    """returns city bt its city id"""
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    return jsonify(city.to_dict())
+
+
+@app_views.route('/cities/<city_id>', methods=['DELETE'])
+def delete_city(city_id):
+    """deletes city by its city id"""
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    storage.delete(city)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'])
+def create_city(state_id):
+    """creates city in a state using the state id"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    data['state_id'] = state_id
+    city = City(**data)
+    city.save()
+    return jsonify(city.to_dict()), 201
+
+
+@app_views.route('/cities/<city_id>', methods=['PUT'])
+def update_city(city_id):
+    """updates city using the city id"""
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    ignore_keys = ['id', 'state_id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if ket not in ignore_keys:
+            setattr(city, key, value)
+    city.save()
+    return jsonify(city.to_dict()), 200
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,11 +7,13 @@ from flask import abort, jsonify, request
 from models import storage
 from models.state import State
 
+
 @app_views.route('/states', methods=['GET'])
 def get_states():
     """Retrieve the list of all State objects."""
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
+
 
 @app_views.route('/states/<state_id>', methods=['GET'])
 def get_state(state_id):
@@ -20,6 +22,7 @@ def get_state(state_id):
     if not state:
         abort(404)
     return jsonify(state.to_dict())
+
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
@@ -31,17 +34,19 @@ def delete_state(state_id):
     storage.save()
     return jsonify({}), 200
 
+
 @app_views.route('/states', methods=['POST'])
 def create_state():
     """Creates a State object from json data."""
     data = request.get_json()
-    if  data is None:
+    if data is None:
         abort(400, 'Not a JSON')
     if 'name' not in data:
         abort(400, 'Missing name')
     state = State(**data)
     state.save()
     return jsonify(state.to_dict()), 201
+
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
 def update_state(state_id):
@@ -57,4 +62,4 @@ def update_state(state_id):
         if key not in ignore_keys:
             setattr(state, key, value)
     state.save()
-    return jsonify(state.to_dict()), 200    
+    return jsonify(state.to_dict()), 200
